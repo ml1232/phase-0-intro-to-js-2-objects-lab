304ea489e730a4c067130f409c7ac1f619ad66a9
Matthew <matthewjaredlevin88@gmail.com>
2023-06-03 12:36:53 -0500

    incomplete = not finished

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your solution in this file!
+const name = "employee name";
+const address = "employee address"
+
+const employee = {
+    [name]: "Matt",
+    [address]: "Valley Dale Ct",
+};
